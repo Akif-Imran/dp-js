1959880c0649dd3bdd84cd05c9dd8d417a74f0b5
Akif-Imran <raiden3322@gmail.com>
2024-08-23 14:17:46 +0500

    bestSum tabulation

--- index.ts
+++ index.ts
@@ -1,11 +1,18 @@
 // snake-case represent memoization functions to solve the problem.
 //camel-case represents tabulation approach function to the same problem
 
-import { canSum, fib, gridTraveler, howSum } from "./src";
+import { bestSum } from "./src";
 
-console.log(fib(8));
-console.log(gridTraveler(5, 5));
-console.log(howSum(7, [5, 3, 4, 7]));
-console.log(howSum(7, [2, 4]));
-console.log(howSum(8, [5, 3, 4, 7]));
-console.log(howSum(2, [5, 3, 4, 7]));
+// console.log(fib(8));
+// console.log(gridTraveler(5, 5));
+// console.log(howSum(7, [5, 3, 4, 7]));
+// console.log(howSum(7, [2, 4]));
+// console.log(howSum(8, [5, 3, 4, 7]));
+// console.log(howSum(2, [5, 3, 4, 7]));
+console.log(bestSum(8, [2, 5, 3, 4, 7]));
+console.log(bestSum(5, [2, 5, 3, 4, 7]));
+console.log(bestSum(6, [2, 5, 3]));
+console.log(bestSum(7, [2, 5, 3, 4, 7]));
+console.log(bestSum(0, [2, 5, 3, 4, 7]));
+console.log(bestSum(100, [1, 2, 5, 25]));
+console.log(bestSum(100, [25, 1, 2, 5]));
--- src/tabulation/number-problems/index.ts
+++ src/tabulation/number-problems/index.ts
@@ -2,3 +2,4 @@ export * from "./fib";
 export * from "./gridTraveler";
 export * from "./canSum";
 export * from "./howSum";
+export * from "./bestSum";
