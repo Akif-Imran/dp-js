09d68c3540f82769a30dc3bf404a20d374078f14
Akif-Imran <raiden3322@gmail.com>
2024-09-05 08:38:02 +0500

    howSum tabulation

--- index.ts
+++ index.ts
@@ -25,21 +25,37 @@ const howSum = (
   return null;
 };
 
+const howSum_tabulation = (target: number, numbers: number[]): number[] | null => {
+  const table = Array(target + 1).fill(null);
+  table[0] = [];
+
+  for (let i = 0; i <= target; i++) {
+    for (let num of numbers) {
+      if (table[i] === null) continue;
+      const index = i + num;
+
+      if (index > target) continue;
+      table[index] = [...table[i], num];
+    }
+  }
+
+  return table[target];
+};
 // console.log(fib(8));
 // console.log(gridTraveler(5, 5));
-console.log(howSum(7, [5, 3, 4, 7]));
-console.log(howSum(7, [2, 4]));
-console.log(howSum(8, [5, 3, 4, 7]));
-console.log(howSum(2, [5, 3, 4, 7]));
-console.log(howSum(7, [5, 3, 4, 7]));
-console.log(howSum(8, [2, 5, 3, 4, 7]));
-console.log(howSum(5, [2, 5, 3, 4, 7]));
-console.log(howSum(6, [2, 5, 3]));
-console.log(howSum(7, [2, 5, 3, 4, 7]));
-console.log(howSum(8, [2, 5, 3, 4, 7]));
-console.log(howSum(103, [1, 2, 5, 25]));
-console.log(howSum(100, [25, 1, 2, 5]));
-console.log(howSum(300, [7, 14]));
+console.log(howSum_tabulation(7, [5, 3, 4, 7]));
+console.log(howSum_tabulation(7, [2, 4])); //null
+console.log(howSum_tabulation(8, [5, 3, 4, 7]));
+console.log(howSum_tabulation(2, [5, 3, 4, 7])); //null
+console.log(howSum_tabulation(7, [5, 3, 4, 7]));
+console.log(howSum_tabulation(8, [2, 5, 3, 4, 7]));
+console.log(howSum_tabulation(5, [2, 5, 3, 4, 7]));
+console.log(howSum_tabulation(6, [2, 5, 3]));
+console.log(howSum_tabulation(7, [2, 5, 3, 4, 7]));
+console.log(howSum_tabulation(8, [2, 5, 3, 4, 7]));
+console.log(howSum_tabulation(103, [1, 2, 5, 25]));
+console.log(howSum_tabulation(100, [25, 1, 2, 5]));
+console.log(howSum_tabulation(300, [7, 14]));
 // console.log(allConstruct("", ["ab", "abc", "cd", "def", "abcd"]));
 // console.log(allConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"]));
 // console.log(allConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd", "ef", "c"]));
